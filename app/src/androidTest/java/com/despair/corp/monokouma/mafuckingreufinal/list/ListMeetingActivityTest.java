81dfea72ba6806bf60ece69688251b72d617193c
monokouma <haaxiz@gmail.com>
2022-11-01 18:27:24 +0100

    first commit

--- app/src/androidTest/java/com/despair/corp/monokouma/mafuckingreufinal/list/ListMeetingActivityTest.java
+++ app/src/androidTest/java/com/despair/corp/monokouma/mafuckingreufinal/list/ListMeetingActivityTest.java
@@ -1,4 +1,4 @@
-package com.despair.corp.monokouma.mafuckingreufinal.create;
+package com.despair.corp.monokouma.mafuckingreufinal.list;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
